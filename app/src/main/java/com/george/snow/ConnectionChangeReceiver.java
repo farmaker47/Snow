0824a643a303533b72af0867b00dd1c1a2bbe930
farmaker47 <farmaker47@yahoo.cm>
2017-12-07 18:28:24 +0200

    bug fixes

--- app/src/main/java/com/george/snow/ConnectionChangeReceiver.java
+++ app/src/main/java/com/george/snow/ConnectionChangeReceiver.java
@@ -17,10 +17,12 @@ public class ConnectionChangeReceiver extends BroadcastReceiver {
     private static final String STOP_TIME = "stop_time";
 
     Context context;
-    public ConnectionChangeReceiver(){}
 
-    public ConnectionChangeReceiver(Context context){
-        this.context=context;
+    public ConnectionChangeReceiver() {
+    }
+
+    public ConnectionChangeReceiver(Context context) {
+        this.context = context;
     }
 
     @Override
@@ -37,34 +39,36 @@ public class ConnectionChangeReceiver extends BroadcastReceiver {
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
 
-        if(isConnected){
-            Toast.makeText(context, "Active WiFi" , Toast.LENGTH_LONG).show();
+        if (isConnected) {
+            Toast.makeText(context, "Active WiFi", Toast.LENGTH_LONG).show();
             long stopTime = System.currentTimeMillis();
 
             //Using SharedPreferences to save stop time
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putLong(STOP_TIME,stopTime);
+            editor.putLong(STOP_TIME, stopTime);
             editor.apply();
 
             //Check if app is in foreground
-            if(mainActivity.isActive==true || secondActivity.isActive==true){
+            if (mainActivity.isActive == true || secondActivity.isActive == true) {
                 cronometerVariable.getStopTime();
 
                 cronometerVariable.sendOnLine();
             }
 
-        }else{
+        } else {
             Toast.makeText(context, "NOT Active WiFi", Toast.LENGTH_LONG).show();
             long startTime = System.currentTimeMillis();
 
             //Using SharedPreferences to save start time
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putLong(START_TIME,startTime);
+            editor.putLong(START_TIME, startTime);
             editor.apply();
 
-            cronometerVariable.setStartTime();
+            if (mainActivity.isActive == true || secondActivity.isActive == true) {
+                cronometerVariable.setStartTime();
+            }
         }
 
     }
--- app/src/main/java/com/george/snow/MainActivity.java
+++ app/src/main/java/com/george/snow/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-
+        Log.e("MainPause", "MainPause");
         //set true if app is running
         isActive=false;
 
